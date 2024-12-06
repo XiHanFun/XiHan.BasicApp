fffef934968bea1f8b88c83ae70eaf76a4b60ba5
zhaifanhua <zhaifanhua@foxmail.com>
2024-12-07 06:43:16 +0800

    优化

---
+++ backend/src/modules/XiHan.App.CodeGeneration.Application/XiHanCodeGenerationApplicationModule.cs
@@ -0,0 +1,35 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2024 ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:XiHanCodeGenerationApplicationModule
+// Guid:706325c3-33e8-4710-8128-f1ee449ffc27
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2024/12/7 6:36:28
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using XiHan.Framework.Core.Modularity;
+using XiHan.Framework.Ddd.Application;
+
+namespace XiHan.App.CodeGeneration.Application;
+
+/// <summary>
+/// XiHanCodeGenerationApplicationModule
+/// </summary>
+[DependsOn(
+    typeof(XiHanDddApplicationModule)
+    )]
+public class XiHanCodeGenerationApplicationModule : XiHanModule
+{
+    /// <summary>
+    /// 服务配置
+    /// </summary>
+    /// <param name="context"></param>
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+}
---
+++ backend/src/modules/XiHan.App.Rbac.Domain.Shared/XiHanRbacDomainSharedModule.cs
@@ -0,0 +1,35 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2024 ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:XiHanRbacDomainSharedModule
+// Guid:e8ce7154-a48c-4934-b548-96673cd857e0
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2024/12/7 6:26:04
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using XiHan.Framework.Core.Modularity;
+using XiHan.Framework.Ddd.Domain.Shared;
+
+namespace XiHan.App.Rbac.Domain.Shared;
+
+/// <summary>
+/// XiHanRbacDomainSharedModule
+/// </summary>
+[DependsOn(
+    typeof(XiHanDddDomainSharedModule)
+    )]
+public class XiHanRbacDomainSharedModule : XiHanModule
+{
+    /// <summary>
+    /// 服务配置
+    /// </summary>
+    /// <param name="context"></param>
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+}
