ebc3f1a4c0552f919d7ecc1a38a79e3460668096
zhaifanhua <zhaifanhua@foxmail.com>
2026-03-04 02:28:38 +0800

    feat: 优化

--- backend/src/modules/XiHan.BasicApp.Rbac/Domain/DomainServices/Implementations/AuthorizationDomainService.cs
+++ backend/src/modules/XiHan.BasicApp.Rbac/Domain/DomainServices/Implementations/AuthorizationDomainService.cs
@@ -12,51 +12,6 @@
 
 #endregion <<版权版本注释>>
 
-
-#region <<版权版本注释>>
-
-// ----------------------------------------------------------------
-// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-// FileName:AuthorizationDomainService
-// Guid:fa6a130f-a199-492d-b1d7-e34f0e1e1c7b
-// Author:zhaifanhua
-// Email:me@zhaifanhua.com
-// CreateTime:2026/02/28 06:59:06
-// ----------------------------------------------------------------
-
-#endregion <<版权版本注释>>
-
-
-#region <<版权版本注释>>
-
-// ----------------------------------------------------------------
-// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-// FileName:AuthorizationDomainService
-// Guid:fa6a130f-a199-492d-b1d7-e34f0e1e1c7b
-// Author:zhaifanhua
-// Email:me@zhaifanhua.com
-// CreateTime:2026/02/28 06:59:06
-// ----------------------------------------------------------------
-
-#endregion <<版权版本注释>>
-
-
-#region <<版权版本注释>>
-
-// ----------------------------------------------------------------
-// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-// FileName:AuthorizationDomainService
-// Guid:fa6a130f-a199-492d-b1d7-e34f0e1e1c7b
-// Author:zhaifanhua
-// Email:me@zhaifanhua.com
-// CreateTime:2026/02/28 06:59:06
-// ----------------------------------------------------------------
-
-#endregion <<版权版本注释>>
-
 namespace XiHan.BasicApp.Rbac.Domain.DomainServices.Implementations;
 
 /// <summary>
